9ba09ef6dd757ea325da5ae4a891f9e72c57dcbc
Adrian <adrcichy@outlook.com>
2019-05-20 20:47:40 +0200

    models added

---
+++ NIP.API/Models/HeaderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NIP.API.Models
+{
+	public class HeaderModel
+	{
+		public int Id { get; set; }
+		public int QueryId { get; set; }
+		public string HeaderName { get; set; }
+		public string HeaderValue { get; set; }
+	}
+}
