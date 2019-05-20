2d434099035059f9727fd61e74f53adbd1db5ef1
Adrian <adrcichy@outlook.com>
2019-05-20 21:00:13 +0200

    company controller added

---
+++ NIP.API/Controllers/CompanyController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NIP.API.Helpers;
+using NIP.API.Models;
+
+namespace NIP.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CompanyController : ControllerBase
+	{
+		[HttpGet]
+		public async Task<IActionResult> GetCompany([FromQuery] FilterParams filterParams)
+		{
+			var mock = new CompanyModel
+			{
+				Id = 1,
+				City = "Poznan",
+				Name = "JanuszSoft",
+				NationalBusinessRegistryNumber = "123456789",
+				NationalCourtRegister = "0000123456",
+				TaxNumber = "0987654321",
+				PostalCode = "11-098",
+				Street = "Poznanska",
+				StreetNumber = "1"
+			};
+
+			return this.Ok(mock);
+		}
+	}
+}
