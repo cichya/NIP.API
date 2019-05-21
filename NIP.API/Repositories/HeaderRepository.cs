17a1749c7d3d15d3b4ef642425b2c84f300421a4
Adrian Cichy <adrcichy@outlook.com>
2019-05-21 17:58:28 +0200

    header repository added

---
+++ NIP.API/Repositories/HeaderRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NIP.API.Data;
+using NIP.API.Models;
+
+namespace NIP.API.Repositories
+{
+	public class HeaderRepository : IHeaderRepository
+	{
+		private readonly DataContext dataContext;
+
+		public HeaderRepository(DataContext dataContext)
+		{
+			this.dataContext = dataContext;
+		}
+
+		public async Task<HeaderModel> Add(HeaderModel header)
+		{
+			await this.dataContext.AddAsync(header);
+
+			return header;
+		}
+
+		public async Task<bool> SaveAll()
+		{
+			return await this.dataContext.SaveChangesAsync() > 0;
+		}
+	}
+}
