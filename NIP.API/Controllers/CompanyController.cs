2d79240c2bcd4f94ce027c0776706df5d9a4e073
Adrian Cichy <adrcichy@outlook.com>
2019-05-22 20:42:55 +0200

    services added to controller

--- NIP.API/Controllers/CompanyController.cs
+++ NIP.API/Controllers/CompanyController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using NIP.API.Helpers;
 using NIP.API.Models;
 using NIP.API.Repositories;
+using NIP.API.Services;
 
 namespace NIP.API.Controllers
 {
@@ -15,15 +16,48 @@ namespace NIP.API.Controllers
 	public class CompanyController : ControllerBase
 	{
 		private readonly ICompanyRepository companyRepository;
+		private readonly IQueryRepository queryRepository;
+		private readonly IHeaderRepository headerRepository;
+		private readonly IHeaderService headerService;
+		private readonly IQueryService queryService;
 
-		public CompanyController(ICompanyRepository companyRepository)
+		public CompanyController(ICompanyRepository companyRepository,
+			IQueryRepository queryRepository,
+			IHeaderRepository headerRepository,
+			IHeaderService headerService,
+			IQueryService queryService)
 		{
 			this.companyRepository = companyRepository;
+			this.queryRepository = queryRepository;
+			this.headerRepository = headerRepository;
+			this.headerService = headerService;
+			this.queryService = queryService;
 		}
 
 		[HttpGet]
 		public async Task<IActionResult> GetCompany([FromQuery] FilterParams filterParams)
 		{
+			var query = this.queryService.GetQueryModelFromFilterParams(filterParams);
+
+			query.Headers = new List<HeaderModel>();
+			
+			var headers = this.headerService.GetHeaderEntityFromRequestHeaders(this.Request.Headers);
+
+			foreach (var header in headers)
+			{				
+				query.Headers.Add(header);
+				header.Query = query;
+
+				await this.headerRepository.Add(header);
+			}
+
+			await this.queryRepository.Add(query);
+
+			if (!await this.queryRepository.SaveAll())
+			{
+				return this.StatusCode(500);
+			}
+
 			var company = await this.companyRepository.Get(filterParams);
 
 			if (company == null)
